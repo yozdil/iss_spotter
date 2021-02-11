e073d15da37e3a651f7140c6488511fe59e2f96e
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-11 19:59:55 +0000

    Geo-location Implemented

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIp } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,13 @@ fetchMyIP((error, ip) => {
   }
 
   console.log("It worked! Returned IP:", ip);
+});
+
+fetchCoordsByIp("142.113.88.222", (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned location:", data);
 });
--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const fetchMyIP = (callback) => {
-  // use request to fetch IP address from JSON API
+  // use request to fetch IP address
   request("https://api.ipify.org/?format=json", (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -19,4 +19,28 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIp = (ip, callback) => {
+  // use request to fetch lat & long
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    // if non-200 status, server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    // In the case we get a result we put that in to our callback as an
+    // argument. As we need this argument to be an object we go ahead and create it.
+    let location = {
+      latitude: JSON.parse(body).latitude.toString(),
+      longitude: JSON.parse(body).longitude.toString(),
+    };
+    // Shorthand version: const { latitude, longitude } = JSON.parse(body);
+    // callback(null, { latitude, longitude });
+    callback(null, location);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIp };
