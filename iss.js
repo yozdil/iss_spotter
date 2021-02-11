4b11267a9b182c96b18c1bc0d76cb55045a4c8b3
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-11 19:34:21 +0000

    Fetch my IP implementation

--- iss.js
+++ iss.js
@@ -1,1 +1,22 @@
-// It will contain most of the logic for fetching the data from each API endpoint.
+const request = require("request");
+
+const fetchMyIP = (callback) => {
+  // use request to fetch IP address from JSON API
+  request("https://api.ipify.org/?format=json", (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    // if non-200 status, server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // In the case we get a result we put that in to our callback as an
+    // argument.
+    callback(null, JSON.parse(body).ip);
+  });
+};
+
+module.exports = { fetchMyIP };
